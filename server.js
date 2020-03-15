e476045ce0297d1b2ac453c46427c4d0b8db454b
Roger Esteve <Coconaut7@gmail.com>
2020-03-15 20:17:42 +0100

    Fresh style, ask for user name at start

--- server.js
+++ server.js
@@ -7,12 +7,27 @@ const server = app.listen(3000);
 const io = socketio(server);
 
 let message_history = [];
+let colors = [];
 
 io.on("connection", function(socket) {
     io.to(socket.id).emit('msgh', message_history);
 
     socket.on("send_msg", function(data) {
+        if(!colors[data.user]) {
+            colors[data.user] = get_random_color();
+        }
+        data.color = colors[data.user];
         message_history.push(data);
         socket.broadcast.emit("receive_msg", data);
+        io.to(socket.id).emit("msg_sent", data);
     });
 });
+
+const get_random_color = () => {
+    const max = 200, min = 50;
+    return {
+        r: Math.floor(Math.random()*(max-min)+min),
+        g: Math.floor(Math.random()*(max-min)+min),
+        b: Math.floor(Math.random()*(max-min)+min)
+    };
+}
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,8 @@
-console.log("cacacaca");
-
 let socket;
+
+let username;
+
+let last_msg;
 
 function send() {
     let today = new Date();
@@ -9,22 +11,46 @@ function send() {
     const yy = today.getFullYear();
     let date = ((dd > 9 ? "" : "0") + dd) + '/'+ ((mm > 9 ? "" : "0") + mm) + "/" + yy;
     
+    const hour = today.getHours();
+    const min = today.getMinutes();
+    const sec = today.getSeconds();
+    let time = ((hour > 9 ? "" : "0") + hour) + ':'+ ((min > 9 ? "" : "0") + min);
+
     let msg = {
         val: $("#inp_chat").val(),
-        user: $("#inp_username").val(),
+        user: username,
         date: date,
-        time: today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+        time: time
     }
     console.log(msg);
+    $("#inp_chat").val("");
     socket.emit("send_msg", msg);
-    addMessage(msg);
 }
 
 
 function addMessage(msg) {
+    const colorString = "rgb(" + msg.color.r + ", " + msg.color.g +", " + msg.color.b + ")";
     let el = document.createElement("li");
-    el.innerHTML = "<strong>" + msg.date + " " + msg.time + " " + msg.user + ": <strong>" + msg.val;
+    
+    let m = "";
+    
+    const m_user = "<span id='msg_user' style='color:" + colorString +"'>" + msg.user + "</span>";
+    const m_datetime = "<strong><span id='msg_datetime'>" + msg.date + " " + msg.time + "</span></strong>";
+
+    if(last_msg) {
+        if(last_msg.user === msg.user && last_msg.date === msg.date && last_msg.time === msg.time) {
+            m = msg.val;
+        } else {
+            m = m_user + " " + m_datetime + "<br>" + msg.val
+        }     
+    }
+
+    el.innerHTML = m;
+    
     document.getElementById("chatbox").appendChild(el);
+    $(".scroll").scrollTop($(".scroll")[0].scrollHeight);
+    
+    last_msg = msg;
 }
 
 function server_connect(ip, port) {
@@ -42,11 +68,18 @@ function server_connect(ip, port) {
         });
     });
 
+    socket.on("msg_sent", function(data) {
+        console.log("Messague succesfully sent");
+        addMessage(data);
+    });
+
     socket.on("receive_msg", function(data) {
         addMessage(data);
     });
 }
 
 window.onload = function() {
-    server_connect(window.prompt("IP:", "??.???.???.???"), "3000");
+    const ip = window.prompt("IP:", "??.???.???.???");
+    username = window.prompt("Username:", "");
+    server_connect(ip, "3000");
 };
